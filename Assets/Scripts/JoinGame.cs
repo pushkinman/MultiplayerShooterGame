b3a74558ee3f0798eb7fb6608d0aab78af4dc72d
Anton <anton.pushkin2@gmail.com>
2020-03-08 20:05:42 +0300

    Joining 1

---
+++ Assets/Scripts/JoinGame.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System.Collections.Generic;
+
+public class JoinGame : MonoBehaviour
+{
+    private NetworkManager networkManager;
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+        status.text = "";
+
+        if(matches == null)
+        {
+            status.text = "Couldn't get room list";
+            return;
+        }
+
+        ClearRoomList();
+
+        foreach(MatchInfoSnapshot match in matches)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+            //adding information
+        }
+    }
+
+    private void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+    }
+}
